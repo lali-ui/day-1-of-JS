b1fdbf33695ad6467a3cc18a77fc978a2c9a5b0b
lali-ui <simran.kumari0055@gmail.com>
2025-03-21 23:07:56 +0530

    updated todo-list exercis using loops

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,11 +1,28 @@
-const todoList = [];
+const todoList = ['make dinner', 'wash dishes'];
 
-function addTodo() {
-  const inputElement = document.querySelector('.js-name-input');
-  const name = inputElement.value;
-  
-  todoList.push(name);
-  console.log(todoList);
+renderTodoList();
 
-  inputElement.value ='';
+function renderTodoList(){
+
+  let todoListHTML = '';
+
+  for (let i = 0; i < todoList.length; i++){
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html;
+  }
+  document.querySelector('.js-todo-list')
+  .innerHTML = todoListHTML;
 }
+
+  function addTodo() {
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
+    
+    todoList.push(name);
+    console.log(todoList);
+
+    inputElement.value ='';
+
+    renderTodoList();
+  }
